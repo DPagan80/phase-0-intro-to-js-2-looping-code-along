8a10ce810da7404b42c2d9ba432563aec650e68c
David Pagan <D.Pagan80@me.com>
2023-03-09 17:37:16 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const messNames = [];
+
+function writeCards(names, cards) {
+    for  (let i = 0; i <  names.length; i++)  {
+        messNames.push(`Thank you, ${names[i]}, for the wonderful ${cards} gift!`);
+    }
+    return messNames;
+}
+function countDown(number) {
+    while (number <= 11, number >=0) {
+        console.log(number--);
+    }
+    return number;
+}
